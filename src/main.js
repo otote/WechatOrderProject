14d5ce01015f94ffeaa1beaded9864f646c51bbe
otote <ljb12138@gmail.com>
2019-03-27 16:23:23 +0800

    login调整顺序

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import './assets/style/theme.css'
 import './assets/style/characters.css'
+import router from './router'
 
-Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
