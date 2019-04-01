829bb3513757f8987d26c788ed0e68122edd6015
otote <ljb12138@gmail.com>
2019-04-01 10:40:03 +0800

    登录界面

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,17 +1,16 @@
-<template>
-  <div>
-    <div class="outer_label">
-      <img class="inner_label login_logo" src="../../assets/logo.png">
-    </div>
-    <div class="login_form">
-      <input type="text" class="qxs-ic_user qxs-icon" placeholder="用户名" v-model="userName">
-      <input type="text" class="qxs-ic_password qxs-icon" placeholder="密码" v-model="password">
-      <!--<button class="login_btn el-button el-button&#45;&#45;primary is-round" type="primary" round>登录</button>-->
-      <el-button class="login_btn" @click.native="login" type="primary" round :loading="isBtnLoading">登录</el-button>
-      <div style="margin-top: 10px">
-        <span style="color: #000099;" @click="login">司机账号登陆</span><span style="float: right;color: #A9A9AB">忘记密码？</span>
-      </div>
-    </div>
+<template class="login_template">
+  <div id="login-page" class="login_page">
+    <el-form class="login_form">
+      <el-form-item>
+         <el-input type="text" class="username" placeholder="用户名" v-model="userName"></el-input>
+      </el-form-item>
+      <el-form-item>
+          <el-input type="password" class="password" placeholder="密码" v-model="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+          <el-button class="login_btn" @click.native="login" type="primary" round :loading="isBtnLoading">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
@@ -22,6 +21,10 @@ export default {
       password: '',
       isBtnLoading: false
     }
+  },
+  mounted () {
+    var clientHight = window.innerHeight
+    document.getElementById('login-page').style.height = clientHight + 'px'
   },
   created () {
     if (JSON.parse(localStorage.getItem('user')) && JSON.parse(localStorage.getItem('user')).userName) {
@@ -43,42 +46,50 @@ export default {
       }
       if (!this.password) {
         this.$message.error('请输入密码')
+        return
       }
+      alert(this.userName)
+      alert(this.password)
     }
   }
 }
 </script>
 <style>
+  .login_page{
+    width: 100%;
+    height: 100%;
+    background: url("../../assets/background.png");
+    background-size: 100%;
+    left: 0px;
+    top: 0px;
+  }
+
   .login_form {
-    padding-top: 10%;
+    display: flex;
+    justify-content: center;
+    padding-top: 20%;
     padding-left: 10%;
     padding-right: 10%;
   }
 
-  .qxs-ic_user {
-    /*background: url("../../assets/login/ic_user.png") no-repeat;*/
-    background-size: 13px 15px;
-    background-position: 3%;
-  }
-
-  .qxs-ic_password {
-    /*background: url("../../assets/login/ic_password.png") no-repeat;*/
-    background-size: 13px 15px;
-    background-position: 3%;
-    margin-bottom: 20px;
-  }
-
-  .login_logo {
-    height: 100%;
-  }
-
   .login_btn {
-    width: 100%;
+    margin-left: 20px;
+    width: 100px;
     font-size: 16px;
     background: -webkit-linear-gradient(left, #000099, #2154FA); /* Safari 5.1 - 6.0 */
     background: -o-linear-gradient(right, #000099, #2154FA); /* Opera 11.1 - 12.0 */
     background: -moz-linear-gradient(right, #000099, #2154FA); /* Firefox 3.6 - 15 */
     background: linear-gradient(to right, #000099, #2154FA); /* 标准的语法 */
     filter: brightness(1.4);
+  }
+
+  .username{
+    margin-left: 20px;
+    width: 200px;
+  }
+
+  .password{
+    margin-left: 20px;
+    width: 200px;
   }
 </style>
